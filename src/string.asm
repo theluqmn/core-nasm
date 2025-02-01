ce317da4df7f8af2cd0d192fe11a1c8466446d5c
theluqmn <luqman.yazrie@outlook.com>
2025-02-02 04:59:20 +0800

    working on it to str converter

--- src/string.asm
+++ src/string.asm
@@ -1,12 +1,15 @@
+section .bss
+    int_to_string_buffer: resb 256
+
 section .text
-    global len_string
-    
+    global strlen
+    global int_to_string
+
 strlen:
-    ; initialise registers
+    ; count the length of a string
     push rsi
     mov rdx, 0
 
-    ; loop through each character in the message
     get_string_len:
         inc rdx
         inc rsi
@@ -16,3 +19,36 @@ strlen:
 
     mov rsi, rdx
     ret
+
+int_to_string:
+    ; convert an integer to a string
+    mov rax, rdi
+    mov rcx, 10
+    xor rbx, rbx
+
+    ; handle zero cases
+    test rax, rax
+    jnz int_to_string_loop
+    mov byte [rsi], '0'
+    mov byte [rsi + 1], 0
+    ret
+
+    int_to_string_loop:
+        xor rdx, rdx
+        div rcx
+        add dl, '0'
+        push rdx
+        inc rbx
+
+        test rax, rax
+        jnz int_to_string_loop
+    
+    int_to_string_pop:
+        pop rdx
+        mov [rsi], dl
+        inc rsi
+        dec rbx
+        jnz int_to_string_pop
+
+        mov byte [rsi], 0
+        ret
